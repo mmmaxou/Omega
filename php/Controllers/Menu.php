11a58a802c5d395d7de2eeb02cec92e05bbb1dd8
alex rclens <alex rclens>
2017-05-09 17:30:54 +0200

    menu fini

--- php/Controllers/Menu.php
+++ php/Controllers/Menu.php
@@ -6,5 +6,30 @@
  * Date: 09/05/2017
  * Time: 12:10
  */
+require ('../Models/Menu.php');
+$menu = new Menu();
+
 $donnees_entree = json_decode($_POST["data"] ,  true );
 $donnees_entree = (array)$donnees_entree_std;
+function dump ($data) {
+    echo '<pre>';
+    var_dump($data);
+    echo '<pre>';
+}
+dump($_POST);
+
+$decoded = json_decode($_POST["data"], true);
+dump($decoded);
+
+foreach ($decoded['added'] as $add) {
+    $menu->sendDBmenu($add['name'], $add['parent_menu_id'], $add['create_user_id']);
+}
+
+
+foreach ($decoded['modified'] as $up) {
+    $menu->updateBDmenu($up['id'], $up['name'], $up['updated_user_id'] );
+}
+
+foreach ($decoded['deleted'] as $del) {
+    $menu->deleteBDmenu( $del['id']);
+}
--- php/Models/Menu.php
+++ php/Models/Menu.php
@@ -12,28 +12,25 @@ require_once('../Connexion.php');
 class Menu
 {
     
-    public function sendDBmenu($name, $parent_menu_id, $lang_code, $page_id, $create_user_id, $gallery_id){    
+    public function sendDBmenu($name, $parent_menu_id, $create_user_id){
         $bdd = new Connexion();
         $pdo = $bdd->myPDO();
-        $req = $pdo->prepare('INSERT INTO t_menu(name, parent_menu_id, lang_code, page_id, create_user_id, gallery_id) VALUES(:name, :parent_menu_id, :lang_code, :page_id, :create_user_id, :gallery_id)');
+        $req = $pdo->prepare('INSERT INTO t_menu(name, parent_menu_id, create_user_id) VALUES(:name, :parent_menu_id, :create_user_id)');
         $req->execute(array(
             'name' => $name,
             'parent_menu_id' => $parent_menu_id,
-            'lang_code' => $lang_code,
-            'page_id' => $page_id,
-            'create_user_id' => $create_user_id,
-            'gallery_id' => $gallery_id
+            'create_user_id' => $create_user_id
         ));
     }
 
     public function updateBDmenu($id, $name, $updated_user_id){    
         $bdd = new Connexion();
         $pdo = $bdd->myPDO();
-        $req = $pdo->prepare('UPDATE t_menu SET name = :name, updated_user_id = :upadte_user_id WHERE id = :id');
+        $req = $pdo->prepare('UPDATE t_menu SET name = :name, updated_user_id = :updated_user_id WHERE id = :id');
         $req->execute(array(
-            'id' => $id,
-            'name' => $name,
-            'update_user_id' => $updated_user_id
+            'name'=> $name,
+            'updated_user_id'=> $updated_user_id,
+            'id'=>$id
         ));
     }
 
