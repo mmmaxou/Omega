5f0c9c1b9b64af4884cd28fbc410bfb4b6fe8a42
alex rclens <alex rclens>
2017-05-10 17:27:19 +0200

    debut page

--- php/Controllers/Menu.php
+++ php/Controllers/Menu.php
@@ -6,8 +6,11 @@
  * Date: 09/05/2017
  * Time: 12:10
  */
-require ('../Models/Menu.php');
+require_once ('../Models/Menu.php');
 $menu = new Menu();
+
+require_once ('../Models/Page.php');
+$page = new Page();
 
 $donnees_entree = json_decode($_POST["data"] ,  true );
 $donnees_entree = (array)$donnees_entree_std;
@@ -23,16 +26,20 @@ $decoded = json_decode($_POST["data"], true);
 //dump($decoded);
 
 foreach ($decoded['added'] as $add) {
-    $menu->sendDBmenu($add['name'], $add['parent_menu_id'], $add['create_user_id']);
+    $page_id = $page->sendDBpage($add['name'],null,null);
+    $menu->sendDBmenu($add['name'], $add['parent_menu_id'], $page_id , $add['create_user_id']);
 }
 
 
 foreach ($decoded['modified'] as $up) {
     $menu->updateBDmenu($up['id'], $up['name'], $up['updated_user_id'] );
+    $page->updateBDpage(,$up['name'],null);
 }
 
 foreach ($decoded['deleted'] as $del) {
     $menu->deleteBDmenu( $del['id']);
+    $page->deleteBDpage($del['id']);
+
 }
 
 header('Location:Index.php');
